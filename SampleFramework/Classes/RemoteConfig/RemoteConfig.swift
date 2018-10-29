b448ef2dd21b662a99debb0f2ade94628431d6fe
MJ141826 <mudit.jain@renishaw.com>
2018-10-29 17:33:50 +0530

    repo setup

---
+++ SampleFramework/Classes/RemoteConfig/RemoteConfig.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteConfig.swift
+//  SampleFramework
+//
+//  Created by Mudit Jain on 29/10/18.
+//  Copyright © 2018 Renishaw Plc. All rights reserved.
+//
+
+import Foundation
+
+public class RemoteConfig{
+    public var id = "id"
+    public static let identifier = "identifier"
+    
+    public func config(){
+        print("I am inside config")
+    }
+}
