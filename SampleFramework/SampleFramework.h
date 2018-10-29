41981076bdcf67091ee17269350e8e363a4295d9
MJ141826 <mudit.jain@renishaw.com>
2018-10-29 17:24:42 +0530

    Initial Commit

---
+++ SampleFramework/SampleFramework.h
@@ -0,0 +1,19 @@
+//
+//  SampleFramework.h
+//  SampleFramework
+//
+//  Created by Mudit Jain on 29/10/18.
+//  Copyright © 2018 Renishaw Plc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SampleFramework.
+FOUNDATION_EXPORT double SampleFrameworkVersionNumber;
+
+//! Project version string for SampleFramework.
+FOUNDATION_EXPORT const unsigned char SampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleFramework/PublicHeader.h>
+
+
